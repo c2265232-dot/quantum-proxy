ddda4a6271019f7a004c1d9abefd619a249bd024
c2265232-dot <c2265232@gmail.com>
2026-03-28 15:31:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const fetch = require("node-fetch");
 
+const app = express();
+
+app.get("/", (req,res)=>res.send("Proxy is running"));
+
+app.get("/proxy", async (req, res) => {
+  let url = req.query.url;
+
+  if (!url) return res.send("No URL");
+
+  try {
+    if (!url.startsWith("http")) url = "https://" + url;
+
+    const response = await fetch(url);
+    const text = await response.text();
+
+    res.send(text);
+  } catch (e) {
+    res.send("Error loading site");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running on " + PORT));
