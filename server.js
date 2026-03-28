aff86194f4446265cf4dd22bb1581b75b8d7fe4a
c2265232-dot <c2265232@gmail.com>
2026-03-28 16:14:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,34 +3,34 @@ const fetch = require("node-fetch");
 
 const app = express();
 
-/* TEST ROUTE */
 app.get("/", (req, res) => {
   res.send("Proxy is running");
 });
 
-/* PROXY ROUTE */
 app.get("/proxy", async (req, res) => {
   let url = req.query.url;
 
-  if (!url) {
-    return res.send("No URL provided");
-  }
+  if (!url) return res.send("No URL");
 
   try {
     if (!url.startsWith("http")) {
-      url = "https://" + url;
+      url = "http://" + url;
     }
 
-    const response = await fetch(url);
-    const text = await response.text();
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0"
+      }
+    });
 
-    res.send(text);
+    const body = await response.text();
+
+    res.send(body);
+
   } catch (err) {
     res.send("Error loading site");
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT, () => console.log("Running on " + PORT));
